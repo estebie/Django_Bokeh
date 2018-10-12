c9bb222d00b00187aa7a76750969988ef5a936fd
Steven Joseph Ceniza <ceniza.sjs@sprobe.com>
2018-10-12 08:17:17 +0800

    Integrate interactive slider app to django.

--- datavis/bokehapp/views.py
+++ datavis/bokehapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from . import BasicLine
+from bokeh.embed import server_session
+from bokeh.util import session_id
 
 def index(request):
     context={
@@ -9,3 +11,11 @@ def index(request):
         "cdn_css":BasicLine.cdn_css
     }
     return render(request, 'bokehapp/index.html', context)
+
+def interactive(request):
+    #bokeh_script=autoload_server(None,  url="http://localhost:5006/random-generator")
+    server_script = server_session(None, session_id=session_id.generate_session_id(), url="http://localhost:5006/slider")
+    context = {"graphname":"Sliders",
+            "server_script": server_script,
+    }
+    return render(request, 'bokehapp/interactive.html', context)
